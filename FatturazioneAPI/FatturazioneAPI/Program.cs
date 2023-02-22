6ceded17cf33ada62a9a4eef39fa539153e5bd05
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-02-22 11:12:54 +0100

    fix cors on program

--- FatturazioneAPI/FatturazioneAPI/Program.cs
+++ FatturazioneAPI/FatturazioneAPI/Program.cs
@@ -9,31 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 builder.Services.AddSingleton<DataBase>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "cors",
-                      policy =>
-                      {
-                          policy.WithOrigins()
-                                            .AllowAnyHeader()
-                                            .AllowAnyMethod()
-                                            .AllowAnyOrigin();
-                      });
-});
-
 var app = builder.Build();
-
+app.UseCors(options => options.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin()
+                      );
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors("cors");
 
 app.UseAuthorization();
 
