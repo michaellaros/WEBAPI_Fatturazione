9c82311b6fdc1a05d5f4f88187d59071bc617b2d
MixterKill29 <mixter.kill29@gmail.com>
2023-03-21 09:29:53 +0100

    fix

--- FatturazioneAPI/FatturazioneAPI/Models/ArticoloModel.cs
+++ FatturazioneAPI/FatturazioneAPI/Models/ArticoloModel.cs
@@ -6,13 +6,13 @@
         public string? cod_articolo { get; set; }
         public string desc_articolo { get; set; }
         public decimal prezzo { get; set; }
-        public int quantita { get; set; }
+        public decimal quantita { get; set; }
         public bool flg_isDiscount { get; set; }
         public decimal prezzo_totale_articolo { get { return prezzo * quantita; } }
         public IVAModel? ivaArticolo { get; set; } = null;
 
         public ArticoloModel() { }
-        public ArticoloModel(string cod_articolo,string desc_articolo, decimal prezzo, int quantita, bool flg_isDiscount, IVAModel ivaArticolo)
+        public ArticoloModel(string cod_articolo,string desc_articolo, decimal prezzo, decimal quantita, bool flg_isDiscount, IVAModel ivaArticolo)
         {
             this.cod_articolo= cod_articolo;
             this.desc_articolo = desc_articolo;
@@ -22,7 +22,7 @@
             this.ivaArticolo = ivaArticolo;
         }
     
-    public ArticoloModel(string desc_articolo, decimal prezzo, int quantita, bool flg_isDiscount)
+    public ArticoloModel(string desc_articolo, decimal prezzo, decimal quantita, bool flg_isDiscount)
         {
             this.desc_articolo = desc_articolo;
             this.prezzo = prezzo;
